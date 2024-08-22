6a5780f39cbe213c6a8d3d3f84172784ddc19c2d
LeticiaRon <rongallardoleticia@gmail.com>
2024-08-21 22:41:37 -0300

    Funciones JS
    Agrego validaciones, y la funcionalidad de volver al inicio cuando se inicia sesión.

---
+++ js/login.js
@@ -0,0 +1,35 @@
+// Variables para el formulario y las alertas
+let loginForm = document.getElementById("login-form");
+let alertSuccess = document.getElementById("alert-success");
+let alertError = document.getElementById("alert-danger");
+
+// Función para mostrar la alerta de éxito
+function showAlertSuccess() {
+  alertSuccess.classList.add("show");
+  setTimeout(() => alertSuccess.classList.remove("show"), 500); // Ocultar después de 0.5 segundos
+}
+
+// Función para mostrar la alerta de error
+function showAlertError() {
+  alertError.classList.add("show");
+  setTimeout(() => alertError.classList.remove("show"), 2000); // Ocultar después de 2 segundos
+}
+
+// Manejar el envío del formulario
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Evitar el envío del formulario
+
+  // Variables de usuario y contraseña
+  let usuario = document.getElementById("usuario").value;
+  let password = document.getElementById("password").value;
+
+  // Validación para que no existan campos vacíos
+  if (usuario === "" || password === "") {
+    showAlertError();
+    return;
+  }
+
+  // Redireccionar a la página de inicio si la validación es exitosa
+  showAlertSuccess();
+  setTimeout(() => (window.location.href = "index.html"), 1000); // Redireccionar después de 1 segundo (para ver la alerta)
+});
