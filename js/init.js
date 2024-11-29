08afbc27b856fb2d093f0c7cd6ca1c29847b0da0
florenciagarciacastelli <florenciagarciacastelli25@gmail.com>
2024-11-29 16:10:53 -0300

    cambios middleware

--- js/init.js
+++ js/init.js
@@ -35,3 +35,54 @@ var getJSONData = function(url){
         return result;
     });
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  let token = localStorage.getItem("token");
+  let currentPage = window.location.pathname.split("/").pop(); //Se obtiene nombre del archivo actual
+  const publicPages = ["login.html", "signup.html"]; //Paginas que no necesitan autenticacion 
+
+  if (!token && !publicPages.includes(currentPage)) { //Verifica si el usuario no esta autenticado y esta en una de las paginas que no necesita autenticacion 
+window.location.href = "login.html?redirected=true"; //Redirige al login con indicador de redireccion 
+return; //Detiene cualquier otra ejecucion 
+  }
+
+  //Verificacion de validez del token con el backend 
+
+    if(token) { 
+      validateToken(token).then((isValid) => {
+//Si el token no es valido, redirige al login 
+      if(!isValid && !publicPages.includes(currentPage)) {
+      localStorage.removeItem("token"); //Elimina el token invalido 
+      windiws.location.href = "login.html?redirected=true";
+      }
+      })
+      .catch((error) => { //Si hay un error, redirige al login
+      console.error("Error al validar el token: ", error);
+      localStorage.removeItem("token");
+      window.location.href = "login.html?redirected=true";
+      });
+      }
+
+});
+
+
+//Funcion para validad el token con el backend 
+async function validateToken(token) {
+  try {
+const response = await fetch("http://localhost:3000/validate-token", {
+method: "POST", 
+headers: {
+"Content-Type": "application/json",
+"access-token": token, //Envia el token en el encabezado
+},
+});
+
+//Si el backend devuelve un error el token no es valido
+return response.ok;
+  }
+catch (error) {
+  console.error("Error al validar token: ", error);
+  return false;
+}
+}
